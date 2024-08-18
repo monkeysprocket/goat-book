34a39dee43d3b1879acc54d0c9b118781bd81607
monkeysprocket <mjquinn88@outlook.com>
2024-08-18 21:59:32 +0100

    new URL, view for adding to existing lists. FT passes :-)

--- lists/views.py
+++ lists/views.py
@@ -11,9 +11,15 @@ def home_page(request: HttpRequest) -> HttpResponse:
 def new_list(request: HttpRequest) -> HttpResponse:
     nulist = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=nulist)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{nulist.id}/")
 
 
-def view_list(request: HttpRequest) -> HttpResponse:
-    items = Item.objects.all()
-    return render(request, "list.html", {"items": items})
+def view_list(request: HttpRequest, list_id: int) -> HttpResponse:
+    our_list = List.objects.get(id=list_id)
+    return render(request, "list.html", {"list": our_list})
+
+
+def add_item(request: HttpRequest, list_id: int) -> HttpResponse:
+    our_list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=our_list)
+    return redirect(f"/lists/{our_list.id}/")
