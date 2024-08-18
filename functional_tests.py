b1441bb1ef5f7d968b58060c2f7444077e247b47
monkeysprocket <mjquinn88@outlook.com>
2024-08-18 17:26:33 +0100

    Completed FT. Added new item to list in home page.

--- functional_tests.py
+++ functional_tests.py
@@ -37,16 +37,20 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(
-            any(row.text == "1: Buy Peacock feathers" for row in rows),
-            "New to-doo item did not appear in table",
-        )
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail("Finish the test!")
+        input_box = self.browser.find_element(By.ID, "id_new_item")
+        input_box.send_keys("Use peacock feathers to make a fly")
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
 
         # Satisfied, she goes back to sleep
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,10 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
 
-def home_page(request):
-    return render(request, "home.html")
+def home_page(request: HttpRequest):
+    return render(
+        request,
+        "home.html",
+        {"new_item_text": request.POST.get("item_text", "")},
+    )
