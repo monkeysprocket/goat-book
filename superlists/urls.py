cd381207e6c2073e530d5f834690af04b489f1f2
monkeysprocket <mjquinn88@outlook.com>
2024-08-18 21:22:37 +0100

    added URL, view for starting a new list, moved POST functionality from home_page to new_list

--- superlists/urls.py
+++ superlists/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.urls import path
 
-from lists.views import home_page, view_list
+from lists.views import home_page, new_list, view_list
 
 urlpatterns = [
     path("", home_page, name="home"),
+    path("lists/new", new_list, name="new_list"),
     path("lists/the-only-list-in-the-world/", view_list, name="view_list")
 ]
